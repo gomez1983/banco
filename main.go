848b8ebd7751134cf70039695e85aca1421193c7
Andre Gomez <54462949+gomez1983@users.noreply.github.com>
2024-06-23 17:30:10 -0300

    Primeira aula

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	var titular string = "André"
+	var numeroAgencia int = 589
+	var numeroConta int = 123456
+	var saldo float64 = 125.5
+
+	fmt.Println(titular, numeroAgencia, numeroConta, saldo)
+
+	var titular2 string = "Iris"
+	var numeroAgencia2 int = 590
+	var numeroConta2 int = 123457
+	var saldo2 float64 = 200
+
+	fmt.Println(titular2, numeroAgencia2, numeroConta2, saldo2)
 
 }
