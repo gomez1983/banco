ebbb97efcd2f2ac7c0c68a99334dc792acb5ea7b
Andre Gomez <54462949+gomez1983@users.noreply.github.com>
2024-06-23 17:58:23 -0300

    Primeira struct e formas diferentes de declarar

--- main.go
+++ main.go
@@ -2,20 +2,24 @@ package main
 
 import "fmt"
 
+type ContaCorrente struct {
+	titular       string
+	numeroAgencia int
+	numeroConta   int
+	saldo         float64
+}
+
 func main() {
+	contaDoAndre := ContaCorrente{titular: "André",
+		numeroAgencia: 589,
+		numeroConta:   123456,
+		saldo:         125.5}
 
-	var titular string = "André"
-	var numeroAgencia int = 589
-	var numeroConta int = 123456
-	var saldo float64 = 125.5
+	contaDaBruna := ContaCorrente{"Bruna", 222, 111222, 200}
 
-	fmt.Println(titular, numeroAgencia, numeroConta, saldo)
-
-	var titular2 string = "Iris"
-	var numeroAgencia2 int = 590
-	var numeroConta2 int = 123457
-	var saldo2 float64 = 200
-
-	fmt.Println(titular2, numeroAgencia2, numeroConta2, saldo2)
+	contaDaCarol := ContaCorrente{titular: "Carol", saldo: 125.5}
+	fmt.Println(contaDoAndre)
+	fmt.Println(contaDaBruna)
+	fmt.Println(contaDaCarol)
 
 }
