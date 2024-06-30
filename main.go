7851bb1f99746cb7349ef377ac7cbb343e161614
Andre Gomez <54462949+gomez1983@users.noreply.github.com>
2024-06-30 20:14:12 -0300

    Alterando a visibilidade

--- main.go
+++ main.go
@@ -1,19 +1,13 @@
 package main
 
 import (
-	"banco/clientes"
 	"banco/contas"
 	"fmt"
 )
 
 func main() {
-	clienteAndre := clientes.Titular{"André", "319.438.008-71", "Desenvolvedor Go"}
-	contaDoAndre := contas.ContaCorrente{clienteAndre, 123, 123456, 100}
-	// contaDoAndre := contas.ContaCorrente{Titular: clientes.Titular{
-	// 	Nome:      "André",
-	// 	CPF:       "319.438.008-71",
-	// 	Profissao: "Desenvolvedor"},
-	// 	NumeroAgencia: 123, NumeroConta: 123456, Saldo: 100}
+	contaExemplo := contas.ContaCorrente{}
+	contaExemplo.Depositar(100)
 
-	fmt.Println(contaDoAndre)
+	fmt.Println(contaExemplo.ObterSaldo())
 }
