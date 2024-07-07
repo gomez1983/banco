98487f44cf22199c1b449ced561fe5be17773ab7
Andre Gomez <54462949+gomez1983@users.noreply.github.com>
2024-07-07 18:59:18 -0300

    Criada interface para pagamento

--- main.go
+++ main.go
@@ -5,11 +5,25 @@ import (
 	"fmt"
 )
 
+func PagarBoleto(conta verificarConta, valorDoBoleto float64) {
+	conta.Sacar(valorDoBoleto)
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string
+}
+
 func main() {
 	contaDoAndre := contas.ContaPoupanca{}
 	contaDoAndre.Depositar(100)
-	contaDoAndre.Sacar(55)
+	PagarBoleto(&contaDoAndre, 60)
 
 	fmt.Println(contaDoAndre.ObterSaldo())
+
+	contaDaCarol := contas.ContaCorrente{}
+	contaDaCarol.Depositar(500)
+	PagarBoleto(&contaDaCarol, 400)
+
+	fmt.Println(contaDaCarol.ObterSaldo())
 
 }
